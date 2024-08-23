807780ddd441d5c51cc92bc2cba5cc2de0d6fbd6
Maksim Leskin <Maksim.Leskin@methed.ru>
2024-08-23 18:58:12 +0300

    добавил websocket

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import { readFile } from "node:fs/promises";
 import path from "node:path";
 import url from "node:url";
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
+import { WebSocketServer } from "ws";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,23 +22,25 @@ const loadBuses = async () => {
 
 const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
   const now = DateTime.now().setZone(timeZone);
-  const [hours, minutes] = firstDepartureTime.split(":").map(Number);
+  const [hour, minute] = firstDepartureTime.split(":").map(Number);
 
-  let departure = DateTime.now().set({ hours, minutes }).setZone(timeZone);
+  let departure = DateTime.now()
+    .set({ hour, minute, second: 0, millisecond: 0 })
+    .setZone(timeZone);
 
   if (now > departure) {
     departure = departure.plus({ minutes: frequencyMinutes });
   }
 
   const endOfDay = DateTime.now()
-    .set({ hours: 23, minutes: 59, seconds: 59 })
+    .set({ hour: 23, minute: 59, second: 59 })
     .setZone(timeZone);
 
   if (departure > endOfDay) {
     departure = departure
       .startOf("day")
       .plus({ days: 1 })
-      .set({ hours, minutes });
+      .set({ hour, minute });
   }
 
   while (now > departure) {
@@ -47,7 +50,7 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
       departure = departure
         .startOf("day")
         .plus({ days: 1 })
-        .set({ hours, minutes });
+        .set({ hour, minute });
     }
   }
 
@@ -56,6 +59,8 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
 
 const sendUpdatedData = async () => {
   const buses = await loadBuses();
+  console.log("buses: ", buses);
+  const now = DateTime.now().setZone(timeZone);
 
   const updatedBuses = buses.map((bus) => {
     const nextDeparture = getNextDeparture(
@@ -63,11 +68,16 @@ const sendUpdatedData = async () => {
       bus.frequencyMinutes,
     );
 
+    const timeRemaining = Duration.fromMillis(
+      nextDeparture.diff(now).toMillis(),
+    );
+
     return {
       ...bus,
       nextDeparture: {
         date: nextDeparture.toFormat("yyyy-MM-dd"),
         time: nextDeparture.toFormat("HH:mm:ss"),
+        remaining: timeRemaining.toFormat("hh:mm:ss"),
       },
     };
   });
@@ -92,6 +102,39 @@ app.get("/next-departure", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log("Server running on http://localhost:" + port);
+const wss = new WebSocketServer({ noServer: true });
+const clients = new Set();
+
+wss.on("connection", (ws) => {
+  console.log("WebSocket connection");
+  clients.add(ws);
+
+  const sendUpdates = async () => {
+    try {
+      const updatedBuses = await sendUpdatedData();
+      const sortedBuses = sortBuses(updatedBuses);
+
+      ws.send(JSON.stringify(sortedBuses));
+    } catch (error) {
+      console.error(`Error websocket connection: ${error}`);
+    }
+  };
+
+  const intervalId = setInterval(sendUpdates, 1000);
+
+  ws.on("close", () => {
+    clearInterval(intervalId);
+    clients.delete(ws);
+    console.log("WebSocket closed");
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+server.on("upgrade", (req, socket, head) => {
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit("connection", ws, req);
+  });
 });
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -31,15 +31,39 @@ const renderBusData = (buses) => {
       <td>${bus.startPoint} - ${bus.endPoint}</td>
       <td>${formatDate(nextDepartureDateTimeUTC)}</td>
       <td>${formatTime(nextDepartureDateTimeUTC)}</td>
+      <td>${bus.nextDeparture.remaining}</td>
     `;
 
     tableBody.append(row);
   });
 };
 
+const initWebSocket = () => {
+  const ws = new WebSocket(`ws://${location.host}`);
+
+  ws.addEventListener("open", () => {
+    console.log("WebSocket connection");
+  });
+
+  ws.addEventListener("message", (event) => {
+    const buses = JSON.parse(event.data);
+    renderBusData(buses);
+  });
+
+  ws.addEventListener("error", (error) => {
+    console.log(`WebSocket error: ${error}`);
+  });
+
+  ws.addEventListener("close", () => {
+    console.log(`WebSocket connection close`);
+  });
+};
+
 const init = async () => {
   const buses = await fetchBusData();
   renderBusData(buses);
+
+  initWebSocket();
 };
 
 init();
