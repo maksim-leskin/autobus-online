6c63a957b0aedddbdd6d7336ab40bc17510fb3d2
Maksim Leskin <Maksim.Leskin@methed.ru>
2024-08-23 22:08:47 +0200

    change secure

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -50,7 +50,7 @@ const renderBusData = (buses) => {
 };
 
 const initWebSocket = () => {
-  const ws = new WebSocket(`ws://${location.host}`);
+  const ws = new WebSocket(`wss://${location.host}`);
 
   ws.addEventListener("open", () => {
     console.log("WebSocket connection");
